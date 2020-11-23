d309883a98c3885fc9cfbb4d9eb3b83fa03fde90
skyx23 <72138704+skyx23@users.noreply.github.com>
2020-11-23 15:22:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#ffcdb2",
+    "#ffb4a2",
+    "#e5989b",
+    "#b5838d",
+    "#6d6875",
+    "#cbc0d3",
+    "#efd3d7",
+    "#feeafa" 
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
